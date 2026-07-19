7b6eaa47757c5fe4b06b131e4fbddf94226a4d2d
Ajungmiiin <wjdals3249@naver.com>
2026-07-19 17:45:19 +0900

    feat : 코스 페이지 route 경로 수정

--- src/routes/(authentication)/course.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/(authentication)/course')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/(authentication)/course"!</div>
-}
