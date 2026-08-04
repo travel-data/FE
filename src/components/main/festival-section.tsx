536c567c06dd5ecc3274b6649ead976cbad389cb
Ajungmiiin <wjdals3249@naver.com>
2026-08-04 21:21:29 +0900

    feat : 행사 정보 리스트 API 연동

--- src/components/main/festival-section.tsx
+++ src/components/main/festival-section.tsx
@@ -1,9 +1,14 @@
 import CalendarIcon from '@/assets/icons/calendar-icon.svg?react'
+import { useFestivalsQuery } from '@/hooks/queries/festival'
+import { cn } from '@/lib/utils'
+import { FestivalResponse } from '@/types/festival'
+import { Link } from '@tanstack/react-router'
 import { useTranslation } from 'react-i18next'
 
-// TODO: MOCK 데이터 교체
 function FestivalSection() {
   const { t } = useTranslation('home')
+
+  const { data, isPending } = useFestivalsQuery()
 
   return (
     <section className="py-6 bg-gray-50 flex flex-col items-center justify-center gap-2.5">
@@ -15,9 +20,17 @@ function FestivalSection() {
       </div>
 
       <ul className="w-full flex overflow-x-auto gap-4 px-5">
-        {Array.from({ length: 3 }).map((_, idx) => (
-          <FestivalListItem key={idx} />
-        ))}
+        {isPending &&
+          Array.from({ length: 3 }).map((_, idx) => (
+            <FestivalListItemSkeleton key={idx} />
+          ))}
+        {!isPending &&
+          data &&
+          data?.map((festival) => (
+            <li key={festival.spotId} className="w-[75%] shrink-0">
+              <FestivalListItem {...festival} />
+            </li>
+          ))}
       </ul>
     </section>
   )
@@ -25,23 +38,52 @@ function FestivalSection() {
 
 export default FestivalSection
 
-function FestivalListItem() {
+function FestivalListItem({
+  name,
+  eventEndDate,
+  eventStartDate,
+  img,
+  status,
+  spotId,
+}: FestivalResponse) {
   return (
-    <li className="flex flex-col gap-2 shrink-0 w-[75%]">
+    <Link
+      to="/festival/$festivalId"
+      params={{ festivalId: String(spotId) }}
+      className="flex flex-col gap-2 w-full"
+    >
       <div className="rounded-lg overflow-hidden relative w-full h-47.5">
-        <span className="absolute rounded-full top-4 left-4 px-3 py-0.5 bg-white text-caption font-bold">
-          행사/이벤트
-        </span>
-        <div className="w-full h-full bg-gray-300"></div>
+        <img src={img} alt={name} className="w-full h-full" />
       </div>
 
       <div>
-        <p className="text-label font-bold">2026 봉황대 뮤직 스퀘어</p>
-        <span className="text-caption text-text-subdued flex gap-0.5 items-center">
+        <p className="text-label font-bold">{name}</p>
+        <p className="text-caption text-text-subdued flex gap-0.5 items-center">
           <CalendarIcon />
-          2026-06-05~2026-08-28
-        </span>
+          {eventStartDate}~{eventEndDate}
+          <span className="mx-0.5">·</span>
+          <span
+            className={cn(
+              status === 'ONGOING' ? 'text-brand-primary' : 'text-text-subdued',
+            )}
+          >
+            {status === 'ONGOING' ? '진행 중' : '진행 예정'}
+          </span>
+        </p>
       </div>
-    </li>
+    </Link>
+  )
+}
+
+function FestivalListItemSkeleton() {
+  return (
+    <div className="flex flex-col gap-2 shrink w-[75%]">
+      <div className="rounded-lg bg-gray-300 animate-pulse w-73 h-47.5" />
+
+      <div>
+        <div className="bg-gray-300 w-35.25 h-4 rounded-xs animate-pulse mb-1.5" />
+        <div className="bg-gray-300 w-36 h-4 rounded-xs animate-pulse" />
+      </div>
+    </div>
   )
 }
